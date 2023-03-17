e127b71ce28d62f61a49598c7bb15d1297ff57a3
shivdutt0411 <tiwari041102@gmail.com>
2023-03-17 11:23:55 +0530

    mysql connection

--- server/server.js
+++ server/server.js
@@ -8,6 +8,35 @@ app.use(cors());
 app.use(bodyParser.json({extended: true}));
 
 const port = 3004;
+const express = require('express');
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({extended: true}));
+
+const port = 3004;
+
+const dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'chm'
+    });
+
+// connect to database
+dbConn.connect(); 
+
+// connection configuration
+app.get("/dummy", (req, res) => {
+    res.json({})
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
 
 app.get("/dummy", (req, res) => {
     res.json({})
